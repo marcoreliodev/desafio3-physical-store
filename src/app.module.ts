709f8c917c6dfbe3313d585c1896ab192ab74d9b
marcoreliodev <aurelius.sf@gmail.com>
2025-04-12 15:32:09 -0300

    feat: add ConfigModule for environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { StoresModule } from './stores/stores.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [StoresModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}.local`, '.env'],
+    }),
+    StoresModule,
+  ],
 })
 export class AppModule {}
