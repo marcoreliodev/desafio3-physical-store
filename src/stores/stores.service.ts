c2486b4595aa4109ce1c6b2347ca1fd607ac74a9
marcoreliodev <aurelius.sf@gmail.com>
2025-04-13 20:07:03 -0300

    feat: implement distance calculation for nearby stores within 100 km

--- src/stores/stores.service.ts
+++ src/stores/stores.service.ts
@@ -1,12 +1,16 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { GoogleMapsService } from 'src/services/google-maps.service';
 import { ViaCepService } from 'src/services/via-cep.service';
+import { Store, StoreDocument } from './schemas/store.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class StoresService {
   constructor(
     private readonly viaCepService: ViaCepService,
     private readonly googleMapsService: GoogleMapsService,
+    @InjectModel(Store.name) private storeModel: Model<StoreDocument>,
   ) {}
 
   async listNearbyStoresByCep(cep: string) {
@@ -26,5 +30,57 @@ export class StoresService {
         'Não foi possível recuperar as coordenadas para a localização informada.',
       );
     }
+
+    const radiusInMeters = 100 * 1000; // 100km
+
+    const nearbyStores = await this.storeModel.find({
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [userCoordinates.lng, userCoordinates.lat],
+          },
+          $maxDistance: radiusInMeters,
+        },
+      },
+    });
+
+    if (nearbyStores.length === 0) {
+      throw new NotFoundException('Nenhuma loja encontrada no raio de 100Km.');
+    }
+
+    const origin = userCoordinates;
+
+    const destinations = nearbyStores.map((store) => ({
+      lat: store.latitude,
+      lng: store.longitude,
+    }));
+
+    const distanceMatrix = await this.googleMapsService.getDistanceMatrix(
+      origin,
+      destinations,
+    );
+
+    return nearbyStores.map((store, index) => ({
+      storeID: store.storeID,
+      storeName: store.storeName,
+      takeOutInStore: store.takeOutInStore,
+      shippingTimeInDays: store.shippingTimeInDays,
+      latitude: store.latitude,
+      longitude: store.longitude,
+      address1: store.address1,
+      address2: store.address2,
+      address3: store.address3,
+      city: store.city,
+      district: store.district,
+      state: store.state,
+      type: store.type,
+      country: store.country,
+      postalCode: store.postalCode,
+      telephoneNumber: store.telephoneNumber,
+      emailAddress: store.emailAddress,
+      distance: distanceMatrix[index].distance.text,
+      duration: distanceMatrix[index].duration.text,
+    }));
   }
 }
